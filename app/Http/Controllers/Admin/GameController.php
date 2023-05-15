1717e5ed5e9c761e12da1c3af0d292c1b452f83c
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-15 17:47:58 +0200

    add: controller

---
+++ app/Http/Controllers/Admin/GameController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Game;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $games = Game::all();
+
+        return view('games.index', compact('games'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('games.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(GameRequest $request)
+    {
+        $data = $request->validate();
+
+        $newGames = new Game();
+
+        $newGames->fill($data);
+        $newGames->save();
+
+        return to_route('games.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Game $game)
+    {
+        return view('games.show', compact('game'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Game $game)
+    {
+        return view('games.edit', compact('game'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(GameRequest $request, Game $game)
+    {
+        $game->update($request);
+        return to_route('games.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Game $game)
+    {
+        $game->delete();
+        return to_route('games.index');
+    }
+}
