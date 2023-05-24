7fe46c7f669a57606bfdaf7416a5c988f66dda85
LeoSallu <L.sallustio99@gmail.com>
2023-05-24 17:09:16 +0200

    feat:reviews-seeder

---
+++ database/seeders/ReviewsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Review;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReviewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 0; $i < 20; $i++){
+        $review =new Review();
+        $review->user_review=fake()->sentence();
+        $review->save();
+        }
+    }
+}
