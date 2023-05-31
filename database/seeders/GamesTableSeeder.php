0217a95cc766463db8193631455b4f6ecef68119
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-31 13:25:08 +0200

    fix: table migration

--- database/seeders/GamesTableSeeder.php
+++ database/seeders/GamesTableSeeder.php
@@ -37,14 +37,17 @@ class GamesTableSeeder extends Seeder
 
             $newGame->release_date = $game['release_date'];
             $newGame->slug = Str::slug($newGame->game, '-');
+
+            $newGame->price = rand(1, 99);
             
             // $newGame->games_id= $genre->id;
 
             $newGame->save();
 
 
-            $genre = Genre::inRandomOrder()->take(3);
-            $newGame->genres()->attach($genre->id);
+            $genre = Genre::inRandomOrder()->take(3)->get();
+            // dd($genre);
+            $newGame->genres()->attach($genre[0]->id);
 
             
             $developer = Developer::inRandomOrder()->first();
